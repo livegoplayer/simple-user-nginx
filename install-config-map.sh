159bde1cd7a4978473ddfbdfa9632f37394600f6
xjyplayer <2532917800@qq.com>
2021-05-03 19:47:28 +0800

    init

--- install-config-map.sh
+++ install-config-map.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-kubectl create namespace user
-kubectl create configmap user-nginx-config-map --from-file=conf/nginx -n user
+kubectl create namespace vue-user
+kubectl create configmap user-nginx-config-map --from-file=conf/nginx -n vue-user
